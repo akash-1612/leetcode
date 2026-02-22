3901863f073a49ae72bfd718d9aaba17d330c0c3
Akash K <akashkalimuthu4_bcs27@mepcoeng.ac.in>
2026-02-22 19:45:25 +0530

    Time: 0 ms (100%), Space: 42.5 MB (11.89%) - LeetHub

---
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int smallestEvenMultiple(int n) {
+        if(n==1)
+            return 2;
+        if((n & 1) ==0)
+            return n;
+        else
+            return n*2;
+    }
+}
